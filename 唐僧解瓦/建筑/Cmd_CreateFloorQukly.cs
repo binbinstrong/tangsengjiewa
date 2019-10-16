ef1b41e440cfc6309c0630f15277dc353ead182b
binbinstrong <30513606+binbinstrong@users.noreply.github.com>
2019-10-16 09:04:36 +0800

    一键楼板 19.10.16

--- 唐僧解瓦/建筑/Cmd_CreateFloorQukly.cs
+++ 唐僧解瓦/建筑/Cmd_CreateFloorQukly.cs
@@ -48,23 +48,76 @@ namespace 唐僧解瓦.建筑
             //}, "test");
             //doc.Create.NewFloor()
 
-            var upfaces = beams.Select(m => m.get_Geometry(new Options() {DetailLevel = ViewDetailLevel.Fine}).Getupfaces().First());
-            MessageBox.Show(upfaces.Count().ToString());
+            Transaction temtran = new Transaction(doc, "temtran");
+            temtran.Start();
+            foreach (Element beam in beams)
+            {
+                var joinedelements = JoinGeometryUtils.GetJoinedElements(doc, beam);
 
-            var solids = upfaces.Select(m =>
-                GeometryCreationUtilities.CreateExtrusionGeometry(m.GetEdgesAsCurveLoops(), XYZ.BasisZ, 10)).ToList();
+                if (joinedelements.Count > 0)
+                {
+                    foreach (var id in joinedelements)
+                    {
+                        var temele = id.GetElement(doc);
+                        var isjoined = JoinGeometryUtils.AreElementsJoined(doc, beam, temele);
+                        if (isjoined)
+                        {
+                            JoinGeometryUtils.UnjoinGeometry(doc, beam, temele);
+                        }
+                    }
+                }
+            }
+            temtran.RollBack();
 
-            MessageBox.Show("solids count"+solids.Count().ToString());
-            var targetsolid = MergeSolids(solids);
+            var solidss = new List<GeometryObject>();//beams.Select(m => m.Getsolids());
+            foreach (var element in beams)
+            {
+                solidss.AddRange(element.Getsolids());
+            }
 
-            List<GeometryObject> geoobjs = solids.Cast<GeometryObject>().ToList();
+            var joinedsolid = MergeSolids(solidss.Cast<Solid>().ToList());
+
+            //var upfaces = beams.Select(m => m.get_Geometry(new Options() { DetailLevel = ViewDetailLevel.Fine }).Getupfaces().First());
+            //MessageBox.Show(upfaces.Count().ToString());
+
+            //var solids = upfaces.Select(m =>
+            //    GeometryCreationUtilities.CreateExtrusionGeometry(m.GetEdgesAsCurveLoops(), XYZ.BasisZ, 10)).ToList();
+
+            //MessageBox.Show("solids count" + solids.Count().ToString());
+            //var targetsolid = MergeSolids(solids);
+
+            //List<GeometryObject> geoobjs = solids.Cast<GeometryObject>().ToList();
+
+            var upfaces = joinedsolid.Getupfaces();
+
+            var edgeArrays = upfaces.First().EdgeLoops.Cast<EdgeArray>().ToList();
+
+            //var curvearrays = curveloops.Select();
+
+            var curveloops = upfaces.First().GetEdgesAsCurveLoops();
+
+            var orderedcurveloops = curveloops.OrderBy(m => m.GetExactLength()).ToList();
+            orderedcurveloops.RemoveAt(orderedcurveloops.Count - 1);
+            curveloops = orderedcurveloops;
+                 
+            var curvearrays = curveloops.Select(m => m.ToCurveArray());
+
 
             doc.Invoke(m =>
             {
-                var directshape = DirectShape.CreateElement(doc, new ElementId(BuiltInCategory.OST_GenericModel));
-                //directshape.AppendShape(new List<GeometryObject>(){targetsolid});
-                directshape.AppendShape(geoobjs);
-            },"temsolid");
+                foreach (var curvearray in curvearrays)
+                {
+                    doc.Create.NewFloor(curvearray, false);
+                }
+            },"一键楼板");
+
+            //doc.Invoke(m =>
+            //{
+            //    var directshape = DirectShape.CreateElement(doc, new ElementId(BuiltInCategory.OST_GenericModel));
+            //    //directshape.AppendShape(new List<GeometryObject>(){targetsolid});
+            //    //directshape.AppendShape(solidss);
+            //    directshape.AppendShape(new List<GeometryObject>() { joinedsolid });
+            //}, "temsolid");
 
             return Result.Succeeded;
         }
@@ -108,7 +161,7 @@ namespace 唐僧解瓦.建筑
     {
         public static List<Face> Getupfaces(this Solid solid)
         {
-            var upfaces =new List<Face>();
+            var upfaces = new List<Face>();
             var faces = solid.Faces;
             foreach (Face face in faces)
             {
@@ -142,10 +195,11 @@ namespace 唐僧解瓦.建筑
             var solids = new List<Solid>();
             if (geoobj is Solid solid)
             {
-                 solids.Add(solid);
+                solids.Add(solid);
             }
             else if (geoobj is GeometryInstance geoInstance)
             {
+                var transform = geoInstance.Transform;
                 var symbolgeometry = geoInstance.SymbolGeometry;
                 var enu = symbolgeometry.GetEnumerator();
                 while (enu.MoveNext())
@@ -166,7 +220,87 @@ namespace 唐僧解瓦.建筑
             return solids;
         }
 
+        public static List<Solid> Getsolids(this GeometryObject geoobj, Transform trs)
+        {
 
+            var solids = new List<Solid>();
+            if (geoobj is Solid solid)
+            {
+                if (trs != null || trs != Transform.Identity)
+                {
+                    solid = SolidUtils.CreateTransformed(solid, trs);
+                }
+                solids.Add(solid);
+            }
+            else if (geoobj is GeometryInstance geoInstance)
+            {
+                var transform = geoInstance.Transform;
+                var symbolgeometry = geoInstance.SymbolGeometry;
+                var enu = symbolgeometry.GetEnumerator();
+                while (enu.MoveNext())
+                {
+                    var temgeoobj = enu.Current as GeometryObject;
+                    solids.AddRange(Getsolids(temgeoobj, transform));
+                }
+            }
+            else if (geoobj is GeometryElement geoElement)
+            {
+                var enu = geoElement.GetEnumerator();
+                while (enu.MoveNext())
+                {
+                    var temgeoobj = enu.Current as GeometryObject;
+                    solids.AddRange(Getsolids(temgeoobj, trs));
+                }
+            }
+            return solids;
+        }
+        /// <summary>
+        /// 从元素获取solids
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public static List<GeometryObject> Getsolids(this Element element)
+        {
+            var result = new List<GeometryObject>();
+
+            //var transform = default(Transform);
+            //if (element is FamilyInstance)
+            //    transform = (element as FamilyInstance).GetTotalTransform();
+            //else transform = Transform.Identity;
+
+            var geometryEle = element.get_Geometry(new Options() { DetailLevel = ViewDetailLevel.Fine });
+
+            var enu = geometryEle.GetEnumerator();
+            while (enu.MoveNext())
+            {
+                var curGeoobj = enu.Current;
+                //if (curGeoobj is Solid solid)
+                //{
+                //    result.Add(solid);
+                //}
+                //else if (curGeoobj is GeometryElement)
+                //{
+                //}
+                //else if (curGeoobj is GeometryInstance geoIns)
+                //{
+                //}
+
+                result.AddRange(curGeoobj.Getsolids(Transform.Identity));
+            }
+
+            return result;
+        }
+
+        public static CurveArray ToCurveArray(this CurveLoop curveloop)
+        {
+            var result = new CurveArray();
+            foreach (Curve c in curveloop)
+            {
+                result.Append(c);
+            }
+
+            return result;
+        }
     }
 
 
